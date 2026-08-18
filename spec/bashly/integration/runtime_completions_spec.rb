e996014f8affe028504b70a6224ab007afd50f32
Danny Ben Shitrit <db@dannyben.com>
2026-08-18 21:48:25 +0300

    - Refactor completions as native runtime functions

---
+++ spec/bashly/integration/runtime_completions_spec.rb
@@ -0,0 +1,32 @@
+require 'open3'
+
+describe 'runtime completions', :slow do
+  workspaces = Dir['spec/fixtures/completions/*'].select { |path| File.directory? path }
+
+  workspaces.each do |workspace|
+    context File.basename(workspace) do
+      examples = YAML.trusted_load_file "#{workspace}/examples.yml"
+      cli = File.expand_path 'spec/tmp/cli'
+
+      before(:context) do
+        reset_tmp_dir
+        FileUtils.cp_r Dir["#{workspace}/*"], 'spec/tmp'
+        Commands::Generate.new.execute %w[generate --quiet]
+      end
+
+      examples.each do |name, example|
+        describe name do
+          it 'works' do
+            stdout, stderr, status = Open3.capture3(
+              cli, '__complete', *example['words']
+            )
+
+            expect(status).to be_success
+            expect(stderr).to be_empty
+            expect(stdout.lines(chomp: true)).to eq example['expected']
+          end
+        end
+      end
+    end
+  end
+end
